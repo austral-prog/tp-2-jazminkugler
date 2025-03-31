23033b2d72b7fc1aa0dd6937d81cdf902bc79f7f
jazminkugler <jkugler@mail.austral.edu.ar>
2025-03-31 15:35:02 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -4,5 +4,6 @@ def ada():
     full_name = f'{first_name} {last_name}'
     print(full_name.lower())
     print(full_name.title())
+    print(full_name.upper())    
     print(f'\t{full_name.lower()}')
 
